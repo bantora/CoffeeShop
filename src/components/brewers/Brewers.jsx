6869d6ee0cd47a445590fc89e3f4f095db009de7
Ryan Paolo Joson <67187687+bantora@users.noreply.github.com>
2023-12-17 01:45:19 +0800

    Card (#4)
    * chore: about section content
    
    * feature: card component

--- src/components/brewers/Brewers.jsx
+++ src/components/brewers/Brewers.jsx
@@ -1,9 +1,18 @@
+import "./brewers.css";
 import "../../App.css";
+
+import mokapot from "../../assets/mokapot.png";
+
+import { Card } from "../reusable";
 
 const Brewers = () => {
   return (
     <div id='brewers' className='brewers section'>
-      Brewers
+      <Card title='Moka pot' img={mokapot}>
+        The Moka pot, a stovetop espresso maker invented in the 1930s by Alfonso
+        Bialetti, is famous for its iconic design and ability to brew strong,
+        aromatic coffee.
+      </Card>
     </div>
   );
 };
