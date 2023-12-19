d0045f206dbba4b097ec2e10d38159157c24fd8c
Ryan Paolo Joson <bantora.1011@gmail.com>
2023-12-19 19:03:50 +0800

    feature: about section and snap effect on scroll

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,11 +4,31 @@ import "./about.css";
 const About = () => {
   return (
     <div id='about' className='about section'>
-      <div className='the-why'>
-        <div className='the-why-title'>The Why?</div>
-        This application serves as a coffee project template for a
-        straightforward React project, providing a platform to showcase
-        proficiency in JavaScript and CSS. Designed with simplicity in mind.
+      <div className='rectangle'>
+        <div className='text-container'>
+          <h1>About Us</h1>
+          <p>
+            Welcome to our world of web development, where a dedicated team of
+            innovative developers enhances their skills by creating versatile
+            boilerplate websites. This particular site, crafted with a focus on
+            coffee, exemplifies our commitment to functional elegance and
+            contemporary design. Our mission is to provide clients with robust,
+            user-friendly starting points for their online presence, catering to
+            businesses ranging from local coffee shops to large coffee brands.
+            With each project, we strive to blend engaging design with seamless
+            functionality, turning your digital vision into reality.
+          </p>
+        </div>
+      </div>
+      <div className='squares'>
+        <div className='square the-story'>
+          <div className='text-container'>
+            <h1>The Story.</h1>
+          </div>
+        </div>
+        <div className='square facts'>
+          
+        </div>
       </div>
     </div>
   );
